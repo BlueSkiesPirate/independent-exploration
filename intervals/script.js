90216cea0284bf9112bc175f8e09c15601010122
BlueSkiesPirate <sebastianreyna128@gmail.com>
2023-08-03 16:52:00 -0400

    first commit

---
+++ intervals/script.js
@@ -0,0 +1,10 @@
+// number of seconds that have passed
+let time = 0;
+// reference to the <span> containing the number
+let counterElement = document.getElementById("time");
+
+setInterval(function timeCount(){
+   time++;
+counterElement.innerText = time
+
+}, 1000)
